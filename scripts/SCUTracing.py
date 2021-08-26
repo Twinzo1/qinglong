76f939a8551aff58193efe862050c3b7229b2a8b
Twinzo1 <44726207+Twinzo1@users.noreply.github.com>
2021-08-27 00:17:25 +0800

    Update SCUTracing.py

--- scripts/SCUTracing.py
+++ scripts/SCUTracing.py
@@ -26,7 +26,11 @@ def str2dict(dict_str):
     """
     json_data = json.dumps(dict_str)
     json_str = json.loads(json_data)
-    cookie = dict([item.split("=", 1) for item in json_str.split(";")])
+    if ";" not in json_str:
+        tmp_list = json_str.split("=", 1)
+        cookie = {tmp_list[0]: tmp_list[1]}
+    else:
+        cookie = dict([item.split("=", 1) for item in json_str.split(";")])
     return cookie
   
 def escape2dict(es_str, escapes='\n'):
