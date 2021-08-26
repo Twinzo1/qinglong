e309534ef895ef522f0d440dcce4c950bfab8196
Twinzo1 <44726207+Twinzo1@users.noreply.github.com>
2021-08-26 13:39:02 +0800

    Update enshan.py

--- scripts/enshan.py
+++ scripts/enshan.py
@@ -17,7 +17,16 @@ import json
 import os
 from pyquery import PyQuery as pq
 
-
+def str2dict(dict_str):
+    """
+    等号分号(= ;)型字符串转字典
+    :return: 字典cookie
+    """
+    json_data = json.dumps(dict_str)
+    json_str = json.loads(json_data)
+    cookie = dict([item.split("=", 1) for item in json_str.split(";")])
+    return cookie
+  
 def escape2dict(es_str, escapes='\n'):
     """
     将下面两种形式的字符串分割为字典
